8000bc859cf3a01f11269c02028570d181c974cc
Ashish Shende <ashishpshende@gmail.com>
2024-02-23 03:19:37 +0530

    files added to repo

---
+++ assets/constants/constants.ts
@@ -0,0 +1,37 @@
+
+
+export class KeywordConstants {
+
+  public static AUTH_TOKEN_KEY = 'Authorization';
+  public static SPREADSHEET_ID_KEY ="X-Spreadsheet-Id";
+
+
+  public static LOGGED_IN_STATUS = 'SIGN_IN_STATUS';
+  public static USER_NAME = 'USER_NAME';
+  public static USER_PASSWORD = 'USER_PASSWORD';
+  public static USER_OBJECT = 'USER_OBJECT';
+  public static USER_EMAIL = 'USER_EMAIL';
+  public static USER_ROW_INDEX = 'ROW_INDEX';
+
+
+  public static ROLE_ADMIN = 'Admin';
+  public static ROLE_SUB_DEALDER = 'SubDealer';
+  public static ROLE_DEALER = 'Dealer';
+
+
+  public static PARCEL_STATUS_NEW = 'New';
+  public static PARCEL_STATUS_IN_TRANSIT = 'In-Transit';
+  public static PARCEL_STATUS_SHIPPED = 'Shipped';
+  public static PARCEL_STATUS_DISPATCH = 'Dispatched';
+  public static PARCEL_STATUS_READY_TO_LOAD = 'Read-To-Transit';
+  public static PARCEL_STATUS_DELIVERED = 'Delivered';
+
+
+
+  //APP Language
+
+  public static LOCALIZATION_ENABLED = "LOCALIZATION_ENABLED";
+  public static SELECTED_APP_LANGUAGE =  "SELECTED_APP_LANGUAGE";
+  public static DEFAULT_APP_LANGUAGE =  "DEFAULT_APP_LANGUAGE";
+
+  }
